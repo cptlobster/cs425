61181f2a1126940de26b1da3596c31760960d4fe
Dustin Thomas <stdio@cptlobster.dev>
2023-11-16 17:53:33 -0600

    change stat to status

--- backend/app/models/Package.scala
+++ backend/app/models/Package.scala
@@ -2,7 +2,7 @@ package models
 
 class Package(id: Long,
               dest: Long,
-              stat: String,
+              status: String,
               category: String,
               weight: Float,
               value: Float,
@@ -12,6 +12,6 @@ class Package(id: Long,
 }
 
 object Package {
-  def apply(id: Long, dest: Long, stat: String, category: String, weight: Float, value: Float, vehicle: Long,
-            depot: Long): Package = new Package(id, dest, stat, category, weight, value, vehicle, depot)
+  def apply(id: Long, dest: Long, status: String, category: String, weight: Float, value: Float, vehicle: Long,
+            depot: Long): Package = new Package(id, dest, status, category, weight, value, vehicle, depot)
 }
