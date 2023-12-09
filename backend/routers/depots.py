ac9aa5c242aaf6df2884c5131ba84e52da17b9e3
Dustin Thomas <stdio@cptlobster.dev>
2023-12-09 14:24:31 -0600

    Add queries for depots, timetables

--- backend/routers/depots.py
+++ backend/routers/depots.py
@@ -130,6 +130,48 @@ FROM depots
     return q.read(query)
 
 
+@router.get("/{id}/packages")
+async def get_packages():
+    query = f"""
+SELECT
+    packages.id,
+    packages.dest, 
+    depots.city,
+    packages.stat,
+    packages.wght,
+    packages.declared_value,
+    packages.vehicle_id,
+    packages.depot_id
+FROM packages
+    JOIN depots ON packages.dest = depots.id
+    WHERE depot_id = {id}
+"""
+    return q.read(query)
+
+@router.get("/{id}/vehicles")
+async def get_fleet():
+    query = f"""
+WITH weights AS (
+    SELECT vehicle_id, SUM(wght) AS usage FROM packages GROUP BY vehicle_id
+)
+
+SELECT
+    fleet.id,
+    fleet.vehicle_type,
+    fleet.rng,
+    COALESCE(weights.usage, 0) AS usage,
+    fleet.capacity,
+    fleet.stat,
+    fleet.destination,
+    depots.city
+FROM fleet
+    JOIN depots ON fleet.destination = depots.id
+    LEFT OUTER JOIN weights ON fleet.id = weights.vehicle_id
+    WHERE fleet.destination = {id}
+"""
+    return q.read(query)
+
+
 @router.get("/{id}/nearby")
 async def get_nearby_depots(id):
     query = """"""
--- backend/routers/timetable.py
+++ backend/routers/timetable.py
@@ -7,11 +7,39 @@ router = APIRouter(prefix="/tt", tags=["timetable"])
 
 @router.get("/")
 async def get_timetable():
-    query = """SELECT * FROM timetable"""
+    query = """
+SELECT
+    timetable.id,
+    timetable.fleet_id,
+    timetable.source,
+    departs.city AS source_city,
+    timetable.dest,
+    arrives.city AS dest_city,
+    timetable.departure,
+    timetable.arrival,
+    timetable.diff
+FROM timetable
+    JOIN depots AS departs ON departs.id = timetable.source
+    JOIN depots AS arrives ON arrives.id = timetable.dest
+"""
     return q.read(query)
-
 
 @router.get("/{id}")
 async def get_entry(id):
-    query = f"""SELECT * FROM timetable WHERE id = {id}"""
+    query = f"""
+SELECT
+    timetable.id,
+    timetable.fleet_id,
+    timetable.source,
+    departs.city AS source_city,
+    timetable.dest,
+    arrives.city AS dest_city,
+    timetable.departure,
+    timetable.arrival,
+    timetable.diff
+FROM timetable
+    JOIN depots AS departs ON departs.id = timetable.source
+    JOIN depots AS arrives ON arrives.id = timetable.dest
+WHERE id = {id}
+"""
     return q.read(query)
