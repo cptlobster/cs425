4773f885a8ff5635b8dd1d6c78c030ef086892c3
Dustin Thomas <stdio@cptlobster.dev>
2023-12-09 11:49:12 -0600

    update backend so that fleet and package tables work

--- backend/routers/fleet.py
+++ backend/routers/fleet.py
@@ -7,11 +7,46 @@ router = APIRouter(prefix="/fleet", tags=["fleet", "vehicles"])
 
 @router.get("/")
 async def get_fleet():
-    query = """"""
+    query = """
+WITH weights AS (
+    SELECT vehicle_id, SUM(wght) AS usage FROM packages GROUP BY vehicle_id
+)
+
+SELECT
+    fleet.id,
+    fleet.vehicle_type,
+    fleet.rng,
+    COALESCE(weights.usage, 0) AS usage,
+    fleet.capacity,
+    fleet.stat,
+    fleet.destination,
+    depots.city
+FROM fleet
+    JOIN depots ON fleet.destination = depots.id
+    LEFT OUTER JOIN weights ON fleet.id = weights.vehicle_id
+"""
     return q.read(query)
 
 
 @router.get("/{id}")
 async def get_vehicle(id):
-    query = """"""
+    query = f"""
+WITH weights AS (
+    SELECT vehicle_id, SUM(wght) AS usage FROM packages GROUP BY vehicle_id
+)
+
+SELECT
+    fleet.id,
+    fleet.vehicle_type,
+    fleet.rng,
+    COALESCE(weights.usage, 0) AS usage,
+    fleet.capacity,
+    fleet.stat,
+    fleet.destination,
+    depots.city
+FROM fleet
+    JOIN depots ON fleet.destination = depots.id
+    LEFT OUTER JOIN weights ON fleet.id = weights.vehicle_id
+    WHERE fleet.id = {id}    
+"""
     return q.read(query)
--- backend/routers/packages.py
+++ backend/routers/packages.py
@@ -7,11 +7,36 @@ router = APIRouter(prefix="/packages", tags=["packages"])
 
 @router.get("/")
 async def get_packages():
-    query = """"""
+    query = """
+SELECT
+    packages.id,
+    packages.dest, 
+    depots.city,
+    packages.stat,
+    packages.wght,
+    packages.declared_value,
+    packages.vehicle_id,
+    packages.depot_id
+FROM packages
+    JOIN depots ON packages.dest = depots.id
+"""
     return q.read(query)
 
 
 @router.get("/{id}")
 async def get_package(id):
-    query = """"""
+    query = f"""
+SELECT
+    packages.id,
+    packages.dest, 
+    depots.city,
+    packages.stat,
+    packages.wght,
+    packages.declared_value,
+    packages.vehicle_id,
+    packages.depot_id
+FROM packages
+    JOIN depots ON packages.dest = depots.id
+WHERE packages.id = {id}
+"""
     return q.read(query)
